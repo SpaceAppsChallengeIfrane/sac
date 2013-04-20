2744520750e3c8101f1e56f851735a002ea13a1b
ali elouafiq <alieelouafiq@gmail.com>
2013-04-20 20:08:41 +0000

    Generalizing it

--- parser.py
+++ parser.py
@@ -1,9 +1,10 @@
-import json
-
-json_data = open("py.json")
+import json, sys
+// add retrieve URL and store to file
+json_data = open(sys.argv[1])
 data = json.load(json_data)
-with open('pypy.json', 'w') as outfile:
+with open(sys.argv[2], 'w') as outfile:
 	for d in data:
 		d["loc"]=[float(d.pop("lon")),float(d.pop("lat"))]
 		json.dump(d, outfile)
 		outfile.write("\n")
+// add mongoDb Connection
