73de04c0bd2c241f915e062d62b15acc4b45ed93
ali elouafiq <alieelouafiq@gmail.com>
2013-04-20 20:13:22 +0000

    * commenting the parser, and changing directory structure
    * adding IO files samples

--- dataReader/parser.py
+++ dataReader/parser.py
@@ -1,10 +1,13 @@
 import json, sys
-// add retrieve URL and store to file
+#add retrieve URL and store to file
 json_data = open(sys.argv[1])
 data = json.load(json_data)
 with open(sys.argv[2], 'w') as outfile:
 	for d in data:
+		# Formatting data to be Used By MongoDB
 		d["loc"]=[float(d.pop("lon")),float(d.pop("lat"))]
+		#using line separator
 		json.dump(d, outfile)
+		#Seperating JSON entities
 		outfile.write("\n")
-// add mongoDb Connection
+#add mongoDb Connection
